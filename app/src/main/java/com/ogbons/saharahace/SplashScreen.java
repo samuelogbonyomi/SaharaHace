426772eb6ee5620fec68684a759d72f55783fe4e
SamuelOgbonyomi <samuelogbonyomi@outlook.com>
2019-07-13 12:45:30 +0100

    First Commit

---
+++ app/src/main/java/com/ogbons/saharahace/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.ogbons.saharahace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.make_withdrawal);
+    }
+}
